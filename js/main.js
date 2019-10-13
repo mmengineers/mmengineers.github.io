b8d8187550ba10cef2be5fa9d831b2ab99ecc68f
Dinesh D <dd.dinesh.rajakumar@gmail.com>
2019-10-13 13:25:33 +0530

    Added zoho form popup for Service Request

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@ $(document).ready(function() {
 		$('#navbarResponsive').collapse('hide');
 		$('html,body').animate({scrollTop: head.offset().top - 58 },'slow');
 	});
-});
 
+	var hashtag = window.location.hash.substr(1);
+	if(hashtag == 'success') {
+		alert("Thank you for contacting MM Engineers Trichy, we will get back to you within 24 hours!");
+		window.location.hash = '';
+	}
+});
 
